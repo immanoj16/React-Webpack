91f0fdfe923fbc64915ba6e7750fe7a478a854a5
immanoj16 <mmaharana14@gmail.com>
2017-08-05 18:28:58 +0530

    Add: bootstrap loader and custom sass and jquery

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ var HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 var path = require('path');
 var webpack = require('webpack');
+var bootstrapEntryPoints = require('./webpack.bootstrap.config');
 
 var isProd = process.env.NODE_ENV === 'production';
 var cssDev = ['style-loader', 'css-loader', 'sass-loader'];
@@ -12,10 +13,12 @@ var cssProd = ExtractTextPlugin.extract({
 })
 
 var cssConfig = isProd ? cssProd : cssDev;
+var bootstrapConfig = isProd ? bootstrapEntryPoints.prod : bootstrapEntryPoints.dev;
 
 module.exports = {
     entry: {
         app: './src/app.js',
+        bootstrap: bootstrapConfig
     },
     output: {
         path: path.resolve(__dirname, "dist"),
@@ -34,9 +37,17 @@ module.exports = {
                 use: 'babel-loader'
             },
             {
-                test: /\.png$/,
-                use: 'file-loader'
-            }
+                test: /\.(jpe?g|png|gif|svg)$/i,
+                use: [
+                    'file-loader?name=images/[name].[ext]',
+                    // 'file-loader?name=[name].[ext]&outputPath=images/'
+                    'image-webpack-loader'
+                ]
+            },
+            { test: /\.(woff2?|svg)$/, loader: 'url-loader?limit=10000&name=fonts/[name].[ext]' },
+            { test: /\.(ttf|eot)$/, loader: 'file-loader?name=fonts/[name].[ext]' },
+            // Bootstrap 3
+            { test:/bootstrap-sass[\/\\]assets[\/\\]javascripts[\/\\]/, loader: 'imports-loader?jQuery=jquery' },
         ]
     },
     devServer: {
@@ -57,7 +68,7 @@ module.exports = {
             template: './src/index.html' // Load a custom template (ejs by default see the FAQ for details)
         }),
         new ExtractTextPlugin({
-            filename: "app.css",
+            filename: "/css/[name].css",
             disable: !isProd,
             allChunks: true
         }),
