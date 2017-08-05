bdd61b4ef897abd385445490b5e03aa98290c27f
immanoj16 <mmaharana14@gmail.com>
2017-08-05 17:02:52 +0530

    Add file loader to load images

--- webpack.config.js
+++ webpack.config.js
@@ -32,6 +32,10 @@ module.exports = {
                 test: /\.js$/,
                 exclude: /node_modules/,
                 use: 'babel-loader'
+            },
+            {
+                test: /\.png$/,
+                use: 'file-loader'
             }
         ]
     },
